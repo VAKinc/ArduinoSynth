b400563d88e45711b35fc2750ead7cb767448e22
Nolan <nolan.josephjp@gmail.com>
2022-02-24 10:40:57 +0900

    Documented functions

--- src/core/time.cpp
+++ src/core/time.cpp
@@ -6,10 +6,20 @@ namespace synth{
      * Updates delta time value and returns the updated value;
     */
 
+    /** 
+     * Increments the internal time delta value. Should only be called at the start of loop() in synth.ino
+     * 
+     * @return void
+    */
     void Time::tick(){
         _delta_time = millis();
     }
 
+    /** 
+     * Returns the delta time value.
+     * 
+     * @return time in MS since the beginning of code execution
+    */
     unsigned long Time::deltaTime(){
         return _delta_time;
     }
